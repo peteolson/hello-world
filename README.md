4eaa7faa2d8268910f37a68f2d8efb020151e3e8
pete <pete@peteolson.me>
2020-12-04 14:14:18 -0500

    main.php root file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # hello-world
 made changes...
 more changes and a delete
+12-4-2020
---
+++ main.php
@@ -0,0 +1,8 @@
+<?PHP
+echo "hello-world";
+echo "<br>";
+
+for($i = 0; $i < 11; $i++) {
+  echo $i;
+  echo "<br>";
+}
